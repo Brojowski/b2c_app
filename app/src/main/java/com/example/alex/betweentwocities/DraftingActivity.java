c28ab5fee63b574f2ac48b9690dd172140da48b6
Brojowski <alex.gajowski@gmail.com>
2017-04-04 17:34:16 -0400

    Begin Drafting activity.

---
+++ app/src/main/java/com/example/alex/betweentwocities/DraftingActivity.java
@@ -0,0 +1,43 @@
+package com.example.alex.betweentwocities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class DraftingActivity extends PortraitActivity
+{
+    private IconManager _tileOptionsManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drafting);
+
+        _tileOptionsManager = new IconManager(this, R.id.available_tiles);
+        _tileOptionsManager.setLayoutMode(IconManager.Mode.Draft7);
+
+        ArrayList<BuildingType> draftingTiles = new ArrayList<>();
+        draftingTiles.add(BuildingType.Tavern_Bed);
+        draftingTiles.add(BuildingType.Shop);
+        draftingTiles.add(BuildingType.House);
+        draftingTiles.add(BuildingType.Park);
+        draftingTiles.add(BuildingType.Factory);
+        draftingTiles.add(BuildingType.Tavern_Music);
+        draftingTiles.add(BuildingType.Office);
+        _tileOptionsManager.setIcons(draftingTiles);
+
+        Button doneDraftingBtn = (Button) findViewById(R.id.done_drafting);
+        doneDraftingBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Continue.
+            }
+        });
+    }
+}
